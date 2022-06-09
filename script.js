a03b3be52c36de9ea1e34d9146e37cefbe7e912d
Connor Todd <connortodd@connors-mbp.lan>
2022-06-09 19:15:38 +0100

    part 2 completed

--- script.js
+++ script.js
@@ -29,7 +29,66 @@ for(let i = 0; i < optionButtons.length; i++) {
         const computersChoice = computersChoiceGenerator();
         console.log('userChoice: ', userChoice);
         console.log('computersChoice: ', computersChoice);
-    })
+        findWinner(userChoice, computersChoice);
+    });
+}
+
+function findWinner (userChoice, computersChoice) {
+    
+    // check to see if the computer is the winner
+    const computerSelection = selection.find(
+        function(item) {
+            return item.name === computersChoice
+        });
+    
+        if(computerSelection.beats === userChoice) {
+            console.log('computer wins');
+            updateScores('computer')
+            return;
+        }
+    
+    // check to see if the user is the winner
+    const userSelection = selection.find(
+        function(item) {
+            return item.name === userChoice
+        });
+
+        if(userSelection.beats === computersChoice) {
+            console.log('user wins') 
+            updateScores('user')
+            return;
+        }
+    
+        // if neither if statements run the round is a draw
+    console.log('it is a draw')
+}
+
+
+
+function updateScores (winner) {
+    console.log('updateScores', winner)
+    // if winner === user update the user score
+    if(winner === 'user') {
+        const userScore = document.getElementById('user-score');
+        let userCurrentScore = parseInt(userScore.innerText);
+        const userUpdatedScore = userCurrentScore += 1;
+        userScore.innerText = userUpdatedScore;
+    }
+    // if winner === computer update the computer score
+    if(winner === 'computer') {
+        const computerScore = document.getElementById('computer-score');
+        let computerCurrentScore = parseInt(computerScore.innerText);
+        const userUpdatedScore = computerCurrentScore += 1;
+        computerScore.innerText = userUpdatedScore;
+    }
+}
+
+function resetScores() {
+    const computerScore = document.getElementById('computer-score');
+    const userScore = document.getElementById('user-score');
+
+    computerScore.innerText = 0;
+    userScore.innerText = 0;
 }
 
 // Find a way to randomise the computers choice - function
